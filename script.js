47c3c239311030416c8a7bc55946f8cba7dde5e0
ToadDeath <toddeth1@gmail.com>
2025-08-31 08:21:57 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,70 +1,24 @@
-// Example GeoJSON with two "counties"
-var counties = {
-  "type": "FeatureCollection",
-  "features": [
-    {
-      "type": "Feature",
-      "properties": { "name": "Sample County A" },
-      "geometry": {
-        "type": "Polygon",
-        "coordinates": [[
-          [-105.05,40.26],[-105.05,40.34],[-105.34,40.34],[-105.34,40.26],[-105.05,40.26]
-        ]]
+// Map setup
+var map = L.map('map').setView([39.0, -105.5], 8); // zoomed in 1 level more
+
+// Load actual GeoJSON file
+fetch('colorado_counties.geojson')  // <-- rename file to avoid %
+  .then(response => response.json())
+  .then(data => {
+    L.geoJSON(data, {
+      style: {
+        color: "black",
+        weight: 1,
+        fillColor: "#cccccc",
+        fillOpacity: 0.6
+      },
+      onEachFeature: function (feature, layer) {
+        layer.bindPopup("<b>" + feature.properties.NAME + "</b>");
+        layer.on('click', function () {
+          this.setStyle({
+            fillColor: "#66cc66" // claimed color
+          });
+        });
       }
-    },
-    {
-      "type": "Feature",
-      "properties": { "name": "Sample County B" },
-      "geometry": {
-        "type": "Polygon",
-        "coordinates": [[
-          [-104.8, 39.9],[-105.0, 39.9],[-105.0, 40.1],[-104.8, 40.1],[-104.8, 39.9]
-        ]]
-      }
-    }
-  ]
-};
-
-var map = L.map('map').setView([39.5, -105.5], 7);
-
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  maxZoom: 19,
-}).addTo(map);
-
-function style(feature) {
-  return {
-    fillColor: "lightblue",
-    weight: 2,
-    opacity: 1,
-    color: "darkblue", // border
-    fillOpacity: 0.6
-  };
-}
-
-function onEachFeature(feature, layer) {
-  layer.bindPopup(`
-    <b>${feature.properties.name}</b><br>
-    <button onclick="claimCounty('${feature.properties.name}')">Claim County</button>
-  `);
-  layer._countyName = feature.properties.name;
-}
-
-var countyLayers = {};
-
-var geojson = L.geoJSON(counties, {
-  style: style,
-  onEachFeature: function(feature, layer) {
-    onEachFeature(feature, layer);
-    countyLayers[feature.properties.name] = layer;
-  }
-}).addTo(map);
-
-function claimCounty(name) {
-  var layer = countyLayers[name];
-  if (!layer) return;
-
-  var currentColor = layer.options.fillColor;
-  layer.setStyle({
-    fillColor: currentColor === "orange" ? "lightblue" : "orange"
+    }).addTo(map);
   });
-}
